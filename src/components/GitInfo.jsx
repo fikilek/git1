82d5e54361c04a3f282486c8a18eb1b91a6f9740
fikile kentane <fikilekentane@gmail.com>
2025-04-04 21:23:42 +0200

    Added paragraph 4,5,6

--- src/components/GitInfo.jsx
+++ src/components/GitInfo.jsx
@@ -6,6 +6,9 @@ const GitInfo = () => {
 			<h1>GitInfo</h1>;<p>Git info paragraph1</p>
 			<p>Git info paragraph2</p>
 			<p>Git info paragraph3</p>
+			<p>Git info paragraph4</p>
+			<p>Git info paragraph5</p>
+			<p>Git info paragraph6</p>
 		</>
 	);
 };
